e1c1cf17d72b5351760a622b3d232b11579d36bf
sabinaciritel <sabinaciritel16@yahoo.com>
2025-01-15 19:53:43 +0200

    test

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def numara_vocale(text):
+    vocale = 'aeiouAEIOU'  # Lista de vocale pentru a verifica în ambele cazuri (majuscule și minuscule)
+    contor = 0
+    for caracter in text:
+        if caracter in vocale:
+            contor += 1
+    return contor
+
+# Exemplu de utilizare a funcției
+text_input = " funcționalitatea."
+numar_vocale = numara_vocale(text_input)
+print(f"Numărul de vocale din textul dat este: {numar_vocale}")
